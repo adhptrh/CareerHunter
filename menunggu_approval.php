f6f2a8bb7f886146af0ea52e81fe99116e86917f
Azmy39 <adhikaputrahermanda39@gmail.com>
2021-04-07 13:54:47 +0700

    fix menunggu approval

--- menunggu_approval.php
+++ menunggu_approval.php
@@ -4,7 +4,22 @@ require "conn.php";
 $title = "CareerHunter";
 $appname = "CareerHunter";
 
-require "checkuser.php";
+$user = null;
+
+if (isset($_SESSION["id"])) {
+    if ($_SESSION["role"] == Role::$user) {
+        $userr = new User($conn);
+        $user = $userr->selectDataById($_SESSION["id"])->fetch_assoc();
+    }
+    if ($_SESSION["role"] == Role::$user_perusahaan) {
+        $userr = new UserPerusahaan($conn);
+        $user = $userr->selectDataById($_SESSION["id"])->fetch_assoc();
+    }
+    if ($_SESSION["role"] == Role::$admin) {
+        $userr = new Admin($conn);
+        $user = $userr->selectDataById($_SESSION["id"])->fetch_assoc();
+    }
+}
 
 require "views/head.php";
 ?>
